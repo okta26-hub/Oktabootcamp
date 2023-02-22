899f9a60e99c66c6d60346e2c76395bf2dbd3395
okta26-hub <55014261+okta26-hub@users.noreply.github.com>
2023-02-22 18:21:35 +0700

    Trial & eror PIM
    tinggal dikit masalah radio button

---
+++ add_employee_tanpa_foto_projectqa.py
@@ -0,0 +1,47 @@
+import unittest
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.action_chains import ActionChains
+
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+ 
+    def test_a_success_login(self):
+        # steps
+        self.driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login") # buka situs
+        time.sleep(3)
+        self.driver.find_element(By.NAME, "username").send_keys("Admin") # isi email
+        time.sleep(1)
+        self.driver.find_element(By.NAME, "password").send_keys("admin123") # isi password
+        time.sleep(1)    
+        self.driver.find_element(By.CLASS_NAME, "orangehrm-login-button").click() # button login
+        time.sleep(2)
+        self.driver.find_element(By.XPATH, "//span[text()='PIM']").click()
+        time.sleep(2)
+        self.driver.find_element(By.LINK_TEXT, "Add Employee").click()
+        time.sleep(2)
+        self.driver.find_element(By.NAME, "firstName").send_keys("asus") # isi nama first name
+        time.sleep(1)
+        self.driver.find_element(By.NAME, "middleName").send_keys("tuf") # isi nama first name
+        time.sleep(1)
+        self.driver.find_element(By.NAME, "lastName").send_keys("a15") # isi nama first name
+        time.sleep(1)
+
+        self.driver.find_element(By.CSS_SELECTOR, "button.oxd-button.oxd-button--medium.oxd-button--ghost[data-v-7e88b27e][data-v-4e1a259a]").click() #klik cancel
+        time.sleep(8)
+       
+
+    def tearDown(self):
+        self.driver.quit()
+        
+
+if __name__ == "__main__":
+    unittest.main()
